b2de0e25e1dddd0c69a1a9c579367519c7c502bf
Amit Sides <amit@aramnik.com>
2022-11-18 19:45:58 +0200

    Updated autostart to run docker

--- autostart.sh
+++ autostart.sh
@@ -2,4 +2,4 @@
 
 DATE=`/bin/date +"%Y-%m-%d_%H.%M"`
 cd /home/raspberry/Desktop/futuresboard/
-stdbuf -oL /home/raspberry/.local/bin/futuresboard > logs/futuresboard_${DATE}.log 2>&1
+docker-compose up -d
